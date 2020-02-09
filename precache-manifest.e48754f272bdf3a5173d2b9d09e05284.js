f33deb98363c73667acc68a8924d1246343f716d
Kristian Ekle <kristian.ekle@gmail.com>
2020-02-09 21:17:13 +0100

    Updates

---
+++ precache-manifest.e48754f272bdf3a5173d2b9d09e05284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "023853783ba948783707",
+    "url": "/krekle/static/css/main.6bb98547.chunk.css"
+  },
+  {
+    "revision": "023853783ba948783707",
+    "url": "/krekle/static/js/main.02385378.chunk.js"
+  },
+  {
+    "revision": "63a8f196648076aa1c3d",
+    "url": "/krekle/static/js/1.63a8f196.chunk.js"
+  },
+  {
+    "revision": "03656dd7b4bdb7efe020",
+    "url": "/krekle/static/js/runtime~main.03656dd7.js"
+  },
+  {
+    "revision": "613db7524873a1e021fa6d85f5b5ef97",
+    "url": "/krekle/index.html"
+  }
+];
