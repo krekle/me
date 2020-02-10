69eabd8c3cd6b3df397cd6e035db79b24ff9dbf3
Kristian Ekle <kristian.ekle@gmail.com>
2020-02-10 16:55:33 +0100

    Updates

---
+++ precache-manifest.f7fbf4b17f9fe604de134475735b7374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86f4c431a5dc6011e7ee",
+    "url": "/me/static/css/main.6bb98547.chunk.css"
+  },
+  {
+    "revision": "86f4c431a5dc6011e7ee",
+    "url": "/me/static/js/main.86f4c431.chunk.js"
+  },
+  {
+    "revision": "63a8f196648076aa1c3d",
+    "url": "/me/static/js/1.63a8f196.chunk.js"
+  },
+  {
+    "revision": "f4ae667e0f9864d95541",
+    "url": "/me/static/js/runtime~main.f4ae667e.js"
+  },
+  {
+    "revision": "7f6f16e2f97b5618055ab3e8d405a023",
+    "url": "/me/index.html"
+  }
+];
