cdb02ade0cda797084c5f613d767bbf5caf669f5
Kristian Ekle <kristian.ekle@bouvet.no>
2020-02-11 09:49:17 +0100

    Updates

---
+++ precache-manifest.c18ae1798a42513453fb20c0f7398df8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb12c3ff0da478e5d056510bdd8393e",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "0762ee38b06e46fe34d5",
+    "url": "/me/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "1de9ef963fc4413ad7f0",
+    "url": "/me/static/js/2.0e39eb25.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/me/static/js/2.0e39eb25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0762ee38b06e46fe34d5",
+    "url": "/me/static/js/main.f6a42351.chunk.js"
+  },
+  {
+    "revision": "3b3e5c5c09a5d619d318",
+    "url": "/me/static/js/runtime-main.664f1f63.js"
+  }
+]);
