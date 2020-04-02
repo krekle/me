08175720ccbc956c20e2ef58bcb636db4e5127ec
krekle <kristian.ekle@gmail.com>
2020-04-02 14:10:47 +0200

    Updates

---
+++ precache-manifest.2e2212771fc709dba4fe7ce92c4649ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36372af29675bfd3eae6b8523ff3b5c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59236a875804796a3666",
+    "url": "/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "09319063a3c303d2b1ad",
+    "url": "/static/js/2.b306b77d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b306b77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59236a875804796a3666",
+    "url": "/static/js/main.16686a3f.chunk.js"
+  },
+  {
+    "revision": "15c47654d8a8bb4fd767",
+    "url": "/static/js/runtime-main.730606a3.js"
+  }
+]);
