0e4eb45d66f6bc3744f89867c2a35539eb66dbd5
Kristian Ekle <kristian.ekle@gmail.com>
2023-08-24 20:41:07 +0200

    Updates

---
+++ precache-manifest.c46aa92d673b87a5a9dfd44cc39d7e8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d1ca03d155e637ea64f",
+    "url": "/static/css/main.39eb8d8d.chunk.css"
+  },
+  {
+    "revision": "9d1ca03d155e637ea64f",
+    "url": "/static/js/main.9d1ca03d.chunk.js"
+  },
+  {
+    "revision": "63a8f196648076aa1c3d",
+    "url": "/static/js/1.63a8f196.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "493620552ec3a94c952c4c27dc7ad34e",
+    "url": "/index.html"
+  }
+];
